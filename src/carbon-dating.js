0cd4c0fb7279ddba5f980721c4b4a35447c3387e
Alexander Bychyk <sanya55bychyk@gmail.com>
2022-04-29 00:03:43 +0300

    feat: start carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  console.log(sampleActivity)
+  // if (typeof sampleActivity != 'string') {
+  //   return false;
+  // }
+  if (typeof str === 'string' && typeof +str === 'number') {
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k)
+    return t
+  }
+  return false
+  // if (sampleActivity != parseInt(sampleActivity)) {
+  //   return false;
+  // }
+
 }
 
 module.exports = {
