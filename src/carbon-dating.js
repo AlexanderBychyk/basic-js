d414edcd62520e029136e3783048a3b72ce0fff5
Alexander <sanya55bychyk@gmail.com>
2022-04-29 10:32:17 +0300

    feat: finish carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -19,18 +19,19 @@ const HALF_LIFE_PERIOD = 5730;
  */
 function dateSample(sampleActivity) {
   console.log(sampleActivity)
-  // if (typeof sampleActivity != 'string') {
-  //   return false;
-  // }
-  if (typeof str === 'string' && typeof +str === 'number') {
+  if (typeof sampleActivity === 'string' 
+      && typeof +sampleActivity === 'number' 
+      && !isNaN(+sampleActivity)
+      && +sampleActivity > 0
+      && +sampleActivity < HALF_LIFE_PERIOD) {
     let k = 0.693 / HALF_LIFE_PERIOD;
-    t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k)
+    let t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k)
+    if (t < 0) {
+      return false
+    }
     return t
   }
   return false
-  // if (sampleActivity != parseInt(sampleActivity)) {
-  //   return false;
-  // }
 
 }
 
