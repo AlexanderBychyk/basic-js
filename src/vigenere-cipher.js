de33424947feab609cf0e24e878e9aa021b57f1b
Alexander Bychyk <sanya55bychyk@gmail.com>
2022-05-02 15:44:23 +0300

    feat: finish vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,74 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(bool) {
+    this.bool = bool == false ? false : true
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    this.checkArguments(message, key)
+
+    message = message.toUpperCase();
+    let keystream = key;
+    while (keystream.length < message.length) {
+      keystream += key;
+    }
+    for (let i = 0; i < message.length; i++) {
+      let m = message[i].charCodeAt(0) - 65;
+      if (!(m >= 0 && m <= 25)) {
+        keystream = keystream.slice(0, i) + message[i] + keystream.slice(i);
+      }
+    }
+    keystream = keystream.slice(0, message.length).toUpperCase();
+
+    let encryption = [];
+    for (let i = 0; i < message.length; i++) {
+      let m = message[i].charCodeAt(0) - 65;
+      if (m >= 0 && m <= 25) {
+        let k = keystream[i].charCodeAt(0) - 65;
+        encryption.push(String.fromCharCode(((m + k) % 26) + 65));
+      } else {
+        encryption.push(message[i])
+      }
+    }
+
+    return this.bool ? encryption.join('') : encryption.reverse().join('');
+  }
+  decrypt(encryptedMessage, key) {
+    this.checkArguments(encryptedMessage, key)
+    
+    encryptedMessage = encryptedMessage.toUpperCase();
+    let keystream = key;
+    while (keystream.length < encryptedMessage.length) {
+        keystream += key;
+    }
+    for (let i = 0; i < encryptedMessage.length; i++) {
+        let m = encryptedMessage[i].charCodeAt(0)-65;
+        if (!(m >= 0 && m <= 25)) {
+            keystream = keystream.slice(0, i) + encryptedMessage[i] + keystream.slice(i);
+        }
+    }
+    keystream = keystream.slice(0, encryptedMessage.length).toUpperCase();
+    
+    let encryption = [];
+    for (let i = 0; i < encryptedMessage.length; i++) {
+        let m = encryptedMessage[i].charCodeAt(0)-65;
+        if (m >= 0 && m <= 25) {
+            let k = keystream[i].charCodeAt(0)-65;
+            encryption.push(String.fromCharCode(((m - k + 26) % 26)+65));
+        } else {
+            encryption.push(encryptedMessage[i])
+        }
+    }
+    
+    return this.bool ? encryption.join('') : encryption.reverse().join('');
+  }
+  checkArguments(arg1, arg2) {
+    if (arg1 == undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    if (arg2 == undefined) {
+      throw new Error('Incorrect arguments!');
+    }
   }
 }
 
